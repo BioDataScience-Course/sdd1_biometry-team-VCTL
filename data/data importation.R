d29095631dc98bad0256ad8e6eda34169ddeae67
clement-tourbez <clement.tourbez@umons.ac.be>
2018-11-09 11:07:59 +0100

    merging

--- data/data importation.R
+++ data/data importation.R
@@ -3,4 +3,3 @@ SciViews::R
 (biometry <- read$csv("https://docs.google.com/spreadsheets/d/1UfpZvx1_nd7d10vIMAfGVZ1vWyIuzeiKxPL0jfkNSQM/export?format=csv", locale = locale(decimal_mark = ",")))
 
 write$rds(biometry, file = "./data/biometry.rds", compress = "gz")
-
