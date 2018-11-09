3f3bf4ad14d9b717df38f47e73d5f7413b7019a9
Vincent FLORQUIN <vincent@Orion.local>
2018-11-09 10:46:36 +0100

    essai1

---
+++ data/data importation.R
@@ -0,0 +1,6 @@
+#Data importation
+SciViews::R
+(biometry <- read$csv("https://docs.google.com/spreadsheets/d/1UfpZvx1_nd7d10vIMAfGVZ1vWyIuzeiKxPL0jfkNSQM/export?format=csv", locale = locale(decimal_mark = ",")))
+
+write$rds(biometry, file = "./data/biometry.rds", compress = "gz")
+
