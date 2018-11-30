36f2b682564f813936c467512c931cc4a793b4eb
clement-tourbez <clement.tourbez@umons.ac.be>
2018-11-30 14:24:58 +0100

    correction 4 tests
    clement-tourbez

--- data/data importation.R
+++ data/data importation.R
@@ -2,6 +2,11 @@
 SciViews::R
 (biometry <- read_csv("https://docs.google.com/spreadsheets/d/1UfpZvx1_nd7d10vIMAfGVZ1vWyIuzeiKxPL0jfkNSQM/export?format=csv", locale = locale(decimal_mark = ",")))
 
+biometry %>.%
+  filter(., ! is.na(masse_corr)) -> biometry
+
+biometry$genre <- as.factor(biometry$genre)
+visdat::vis_dat(biometry)
 
 
 write$rds(biometry, file = "./data/biometry.rds", compress = "gz")
