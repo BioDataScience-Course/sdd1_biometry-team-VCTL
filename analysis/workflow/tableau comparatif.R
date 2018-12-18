7e96bbf92da40f59b6c7f004ba59eeb7649faaf0
clement-tourbez <clement.tourbez@umons.ac.be>
2018-12-18 11:34:33 +0100

    add workflow comparaison
    files structuration

--- analysis/workflow/tableau comparatif.R
+++
@@ -1,9 +0,0 @@
----
-title: "Comparaison entre les avantages de Rstdtudio et Word"
----
-
-  | Avantages des rapports graphiques dans Rstudio             | Avantages des rapports graphiques dans Word                 |
-  | ---------------------------------------------------------- | ----------------------------------------------------------- |
-  | L'échelle s'adapte plus facilement aux données             | Pas de commande à retenir c'est plus simple niveau commande |
-| Plus de possiblitités                                      | C'est plus visuel                                           |
-  | Les ordonnées et les abcisses sont plus facile à organiser | A la portée de tout le monde.                               |
